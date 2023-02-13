50af4f717bbb45dec29f2cd7ed8be8e95315c8c2
Neverwinter Nights <71729548+neverwinter-nights@users.noreply.github.com>
2023-02-14 02:56:48 +0300

    Script now removes '.git' folders from the archive.

--- clone-repositories.bat
+++ clone-repositories.bat
@@ -25,10 +25,22 @@ FOR /F "usebackq tokens=*" %%A IN ("%repo_list%") DO (
 )
 CD ..
 
+:: Fetch all repositories.
 FOR /F "usebackq tokens=1,2 delims==" %%i IN (`wmic os get LocalDateTime /VALUE 2^>NUL`) DO IF '.%%i.'=='.LocalDateTime.' SET ldt=%%j
 SET ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2%_%ldt:~8,2%-%ldt:~10,2%
 SET time_text=%ldt%
 
+:: Remove '.git' folders.
+CD %repos_folder%
+FOR /f "tokens=*" %%F IN ('DIR /AD /B') DO (
+	ECHO %%F
+	CD %%F
+	RMDIR /S /Q .git
+	CD ..
+)
+CD ..
+
+:: Compress all the folders.
 SET archive_name=%repos_folder%_%time_text%.7z
 ECHO Compressing "%repos_folder%" into "%archive_name%".
 SET tmp_archive_name=tmp.7z
