f3b420cbb26d5a2bd17b6375d78f6701583e6ea2
Neverwinter Nights <71729548+neverwinter-nights@users.noreply.github.com>
2023-03-02 04:34:47 +0300

    Dependency matrix update.

--- clone-repositories.bat
+++ clone-repositories.bat
@@ -1,8 +1,8 @@
 :: Repository Saver Script
-:: Version 0.6.15.
+:: Version 0.6.16.
 
 @ECHO OFF
-ECHO Repository Saver Script, ver. 0.6.15.
+ECHO Repository Saver Script, ver. 0.6.16.
 
 SET arg_1=%1
 ECHO Argument=[%arg_1%]
